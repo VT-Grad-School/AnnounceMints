6a03b76b57aae225b6d346e6008cbdf2792c4789
jmb2331 <jmb2331@vt.edu>
2015-11-14 22:05:31 -0500

    Using the ng-show and ng-hide angular functions. Allowing links and titles to be pushed to the list by the user.

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ angular.module('flapperNews', [])
         '$scope',
         function($scope) {
 
+            $scope.incrementUpvotes = function(post) {
+                post.upvotes += 1;
+            };
+
             $scope.addPost = function() {
-            	if(!$scope.title || $scope.title === '') { return; }
+                if (!$scope.title || $scope.title === '') {
+                    return;
+                }
                 $scope.posts.push({
                     title: $scope.title,
+                    link: $scope.link,
                     upvotes: 0
                 });
                 $scope.title = '';
+                $scope.link = '';
             };
             $scope.posts = [{
                 title: 'post 1',

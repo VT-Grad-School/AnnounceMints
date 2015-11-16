0488e8f9c4ce19d93fb99dbcb0e14bdaaa19b555
jmb2331 <jmb2331@vt.edu>
2015-11-15 23:38:09 -0500

    Adding a state now that UI router is working.

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ angular.module('flapperNews', ['ui.router'])
 
 //= require angular-ui-router`
 .config([
+
 '$stateProvider',
 '$urlRouterProvider',
+
+
 function($stateProvider, $urlRouterProvider) {
+
+
 
   $stateProvider
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
-    });
+    })
+    .state('posts', {
+  url: '/posts/{id}',
+  templateUrl: '/posts.html',
+  controller: 'PostsCtrl'
+});
 
   $urlRouterProvider.otherwise('home');
+
+
 }])
 
 .factory('posts', [function(){
